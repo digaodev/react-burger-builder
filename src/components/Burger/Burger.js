ea2ebda6455bf550a757663aafa5029deb0ae7a6
digaodev <rodrigo.rb.dev@gmail.com>
2018-02-19 15:24:03 -0300

    Feature: add message when there is no ingredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-  const ingredients = Object.keys(props.ingredients).map(key => {
-    return [...Array(props.ingredients[key])].map((_, index) => {
-      return <BurgerIngredient key={key + index} type={key} />;
-    });
-  });
+  let ingredients = Object.keys(props.ingredients)
+    .map(key => {
+      return [...Array(props.ingredients[key])].map((_, index) => {
+        return <BurgerIngredient key={key + index} type={key} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
 
+  if (ingredients.length === 0) {
+    ingredients = <p>Please add ingredients</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
