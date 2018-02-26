a11cc4f1159e725647b5fa6c2ae22d5f9fc9f9c5
digaodev <rodrigo.rb.dev@gmail.com>
2018-02-26 14:46:49 -0300

    Feature: add a side drawer toggle to show/hide and its menu styles

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,17 +4,29 @@ import classes from './SideDrawer.css';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = props => {
+  let attachedClasses;
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  } else {
+    attachedClasses = [classes.SideDrawer, classes.Close];
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
